3dca42d0ec6348c44ed03e24d6a568faecffc7ef
Axcelion017 <michaelnewton1708@gmail.com>
2026-05-05 15:39:02 +0700

    Latihan

--- Latihan/src/Main.java
+++ Latihan/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         for (PerangkatPintar isi : perangkatPintars){
             isi.aktifkan();
         }
-        //Alasan bisa tercetak beda karena method aktifkan() di LampuPintar dan AcPintar sudah di override dan objek yang dibuat berpacu pada tipe data aktual dari objeknya, sehingga ketika method aktifkan() dipanggil, maka yang tercetak adalah method aktifkan() yang ada di class LampuPintar dan AcPintar, bukan method aktifkan() yang ada di class PerangkatPintar.
+        //Alasan bisa tercetak beda karena method aktifkan() di LampuPintar dan AcPintar sudah di override dan yang terjadi adalah runtime polymorhphism jadi dia mengecek tipe data aktual dari masing masing objek yang tersimpan di array list, sehingga ketika method aktifkan() dipanggil, maka yang tercetak adalah method aktifkan() yang ada di class LampuPintar dan AcPintar, bukan method aktifkan() yang ada di class PerangkatPintar.
 
         if(perangkatPintars.get(1) instanceof AcPintar){
             AcPintar ac = (AcPintar) perangkatPintars.get(1);
@@ -17,7 +17,8 @@ public class Main {
 
         PerangkatPintar alat1 = new LampuPintar();
         //alat1.aturKecerahan(75, "Putih"); //Baris menyebabkan error saat dikompilasi
-        
+        //Analsis: Karena terbentuk dalam tipe data PerangkatPintar, maka hanya method yang ada di class PerangkatPintar yang bisa dipanggil, sedangkan method aturKecerahan() tidak ada di class PerangkatPintar melainkan ada di class LampuPintar. Cara memperbaikinya adalah dengan melakukan downcasting ke tipe data LampuPintar terlebih dahulu sebelum memanggil method aturKecerahan().
+        ((LampuPintar)alat1).aturKecerahan(75, "Putih");
 
     }
 }
